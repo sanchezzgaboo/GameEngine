ce239846104414f4208225b177b50424c8c8a6c9
sanchezzgaboo <gsanchezv07@outlook.es>
2026-01-22 11:57:51 -0500

    cartesian Plane init

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include "imgui/imgui.h"
 #include "imgui/imgui-SFML.h"
+#include <iostream>
 
 unsigned int screenwidth = 600;
 unsigned int screenheight = 600;
@@ -61,6 +62,38 @@ struct mesh{
 
 		return true;
 	}
+
+    bool generateLine(int length, int height){
+        std::vector<sf::Vector3f> verts;
+        for(int i = 0; i < height; i++){ 
+            for(int j = 0; j < length; j++){
+                sf::Vector3f v;
+                v.x = 0;
+                v.y=i;
+                v.z=j;
+                verts.push_back(v);
+            }
+        }
+
+        // Build two triangles per grid cell (quad) avoiding out-of-range accesses.
+        // Iterate rows (height) and columns (length), but stop one short on each
+        // dimension because each cell references the next row/column.
+        if (length > 1 && height > 1) {
+            for (int i = 0; i < height - 1; ++i) {
+                for (int j = 0; j < length - 1; ++j) {
+                    int idx = i * length + j;
+                    // First triangle of the quad
+                    tris.push_back({ verts[idx], verts[idx + length], verts[idx + length + 1] });
+                    // Second triangle of the quad
+                    tris.push_back({ verts[idx], verts[idx + length + 1], verts[idx + 1] });
+                }
+            }
+        }
+
+
+        return true;
+
+    }
 };
 struct mat4x4{
     float m[4][4] = {0};
@@ -224,32 +257,72 @@ int main()
     mat4x4 matProj;
     sf::Vector3f vCamera;
     sf::Vector3f vLookDir;
+    float fYaw;
+    float movementX;
+    float movementY;
+    int prevX;
+    int prevY;
+    bool isPanning = false;
+    sf::Vector2i lastMousePosition;
+    
     
     //load File
-    meshCube.LoadFromObjectFile("VideoShip.obj");
+    meshCube.generateLine(2, 2);
     //projection matrix
     matProj = Matrix_MakeProjection(90.0f, (float)screenheight / (float)screenwidth, 0.1f, 1000.0f);
     float fTheta = 0.0f;
     float rotationSpeed = 0.5f;
     
-
-
+    
     sf::RenderWindow window(sf::VideoMode({screenwidth, screenheight}), "SFML works!");
     // Set the frame rate limit to 60 FPS
     //window.setFramerateLimit(60);
 
+    // get the local mouse position (relative to a window)
+    bool leftButtonPressed = false;
     //Imgui Stuff
     ImGui::SFML::Init(window);
     bool checkboxState = false; // Initial state of the checkbox
+    bool cartesianState = false; // Initial state of the checkbox
+    
+    float mousePanSens = 0.2f;
 
     sf::Clock clock;
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
+            // Pass the polled event to ImGui properly (one event at a time)
             ImGui::SFML::ProcessEvent(window, *event);
             if (event->is<sf::Event::Closed>())
                 window.close();
+            if (const auto* mouseButtonPressed = event->getIf<sf::Event::MouseButtonPressed>())
+            {
+                if (mouseButtonPressed->button == sf::Mouse::Button::Right)
+                {
+                    std::cout << "the right button was pressed" << std::endl;
+                }
+
+                if (mouseButtonPressed->button == sf::Mouse::Button::Middle)
+                {
+                    std::cout << "mouse x: " << mouseButtonPressed->position.x << std::endl;
+                    std::cout << "mouse y: " << mouseButtonPressed->position.y << std::endl;
+                }
+
+                if (mouseButtonPressed->button == sf::Mouse::Button::Left)
+                {
+                    std::cout << "the left button was pressed" << std::endl;
+                    leftButtonPressed = true;
+                }
+                
+
+            }
+            if(const auto* mouseButtonReleased = event->getIf<sf::Event::MouseButtonReleased>()){
+                if (mouseButtonReleased->button == sf::Mouse::Button::Left){
+                    leftButtonPressed = false;
+                }
+            }
+        
         }
 
         sf::Time elapsedTime = clock.restart();
@@ -276,17 +349,39 @@ int main()
             }
             ImGui::EndMainMenuBar();
         }
-
+        
         ImGui::Begin("Debug Console");
         ImGui::Checkbox("Enable WireFrame", &checkboxState);
+        ImGui::Checkbox("Enable Cartesian Plane", &cartesianState);
+
+        ImGui::SliderFloat("Mouse Pan Sensitivity", &mousePanSens, 0.0f, 0.5f);
         ImGui::End();
 
         window.clear();
 
-
+        
         mat4x4 matRotZ, matRotX;
         float deltaTime = elapsedTime.asSeconds();
+        if(!ImGui::IsWindowHovered()){
+            if(leftButtonPressed){
+            if(!isPanning){
+                isPanning = true;
+                lastMousePosition = sf::Mouse::getPosition(window);
+            }
+            else{
+                sf::Vector2i currentMousePosition = sf::Mouse::getPosition(window);
+                sf::Vector2f delta = static_cast<sf::Vector2f>(currentMousePosition-lastMousePosition);
+                vCamera.x -= delta.x * mousePanSens/10;
+                vCamera.y -= delta.y * mousePanSens/10;
+                lastMousePosition = currentMousePosition;
+            }
 
+            }
+            else{
+                isPanning = false;
+            }
+        }
+        
         fTheta += rotationSpeed * deltaTime;
         matRotZ = Matrix_MakeRotationZ(fTheta * 0.5f);
 		matRotX = Matrix_MakeRotationX(fTheta);
@@ -296,7 +391,7 @@ int main()
 
 		mat4x4 matWorld;
 		matWorld = Matrix_MakeIdentity();	// Form World Matrix
-		matWorld = Matrix_MultiplyMatrix(matRotZ, matRotX); // Transform by rotation
+		matWorld = Matrix_MultiplyMatrix(matRotZ, matWorld); // Transform by rotation
 		matWorld = Matrix_MultiplyMatrix(matWorld, matTrans); // Transform by translation
         vLookDir = {0,0,1};
         sf::Vector3f vUp(0,1,0);
@@ -311,7 +406,7 @@ int main()
         for(auto tri: meshCube.tris){
 
 
-            triangle triProjected, triTranslated;
+            triangle triProjected, triTranslated, triViewed;
             triTranslated.p[0]=Matrix_MultiplyVector(matWorld, tri.p[0], false);
             triTranslated.p[1]=Matrix_MultiplyVector(matWorld, tri.p[1], false);
             triTranslated.p[2]=Matrix_MultiplyVector(matWorld, tri.p[2], false);
@@ -344,9 +439,17 @@ int main()
                 triProjected.g *= dp;
                 triProjected.b *= dp;
 
-                triProjected.p[0]=Matrix_MultiplyVector(matProj, triTranslated.p[0], true);
-                triProjected.p[1]=Matrix_MultiplyVector(matProj,triTranslated.p[1], true);
-                triProjected.p[2]=Matrix_MultiplyVector(matProj, triTranslated.p[2], true);
+                //Primero convertir espacio global -> vista camara
+                triViewed.p[0]=Matrix_MultiplyVector(matView, triTranslated.p[0], true);
+                triViewed.p[1]=Matrix_MultiplyVector(matView,triTranslated.p[1], true);
+                triViewed.p[2]=Matrix_MultiplyVector(matView, triTranslated.p[2], true);
+            
+
+
+                //camara -> pantalla
+                triProjected.p[0]=Matrix_MultiplyVector(matProj, triViewed.p[0], true);
+                triProjected.p[1]=Matrix_MultiplyVector(matProj,triViewed.p[1], true);
+                triProjected.p[2]=Matrix_MultiplyVector(matProj, triViewed.p[2], true);
             
                 //offset into visible normalised space
                 sf::Vector3f vOffsetView(1,1,0);
@@ -378,7 +481,7 @@ int main()
         });
         for(auto &triProjected:vecTrianglestoRaster){
             sf::VertexArray currentTriangle(sf::PrimitiveType::Triangles, 3);
-            
+        
             currentTriangle[0].position = sf::Vector2f(triProjected.p[0].x, triProjected.p[0].y);
             currentTriangle[1].position = sf::Vector2f(triProjected.p[1].x, triProjected.p[1].y);
             currentTriangle[2].position = sf::Vector2f(triProjected.p[2].x, triProjected.p[2].y);
@@ -401,12 +504,25 @@ int main()
                 currentWireFrame[3].color = sf::Color::Yellow;
                 window.draw(currentWireFrame);
             }
+            if(cartesianState){
+                // Draw simple Cartesian axes (avoid invalid indices)
+                sf::VertexArray axes(sf::PrimitiveType::Lines, 4);
+                // Horizontal axis
+                axes[0].position = sf::Vector2f(0.0f, (float)screenheight / 2.0f);
+                axes[1].position = sf::Vector2f((float)screenwidth, (float)screenheight / 2.0f);
+                // Vertical axis
+                axes[2].position = sf::Vector2f((float)screenwidth / 2.0f, 0.0f);
+                axes[3].position = sf::Vector2f((float)screenwidth / 2.0f, (float)screenheight);
+                for (int i = 0; i < 4; ++i) axes[i].color = sf::Color::Green;
+                window.draw(axes);
+            }
 
 
             window.draw(currentTriangle);
-            ImGui::SFML::Render(window);
         }
-        
+        // Render ImGui once per frame (after drawing scene, before display)
+        ImGui::SFML::Render(window);
+
         window.display();
     }
     ImGui::SFML::Shutdown();
